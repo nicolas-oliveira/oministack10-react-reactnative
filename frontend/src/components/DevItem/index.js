80ee32c383674c456f5a2d4b7a3ac251e803b917
Nicolas <nicolasm15@hotmail.com>
2020-01-25 21:03:16 -0300

    Front end changes 2

---
+++ frontend/src/components/DevItem/index.js
@@ -0,0 +1,19 @@
+import React  from 'react';
+import './styles.css'
+function DevItem({dev}){
+    //const {dev} = props;
+    return(
+        <li key={dev._id} className="dev-item">
+            <header>
+                <img src={dev.avatar_url} alt={dev.name}></img>
+                <div className="user-info">
+                    <strong>{dev.name}</strong>
+                    <span>{dev.techs.join(',')}</span>
+                </div> 
+            </header>
+            <p>{dev.bio}</p>
+            <a href={`https://github.com/${dev.github_username}`} target="_blank">Acessar perfil no Github</a>
+        </li> 
+    );
+}
+export default DevItem;
